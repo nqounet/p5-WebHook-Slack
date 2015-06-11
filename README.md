920e0c647835d8588e1fc26874a3121b4e93ec86
nqounet <mail@nqou.net>
2015-06-11 23:47:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebHook::Slack - It's new $module
+
+# SYNOPSIS
+
+    use WebHook::Slack;
+
+# DESCRIPTION
+
+WebHook::Slack is ...
+
+# LICENSE
+
+Copyright (C) nqounet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nqounet <mail@nqou.net>
---
+++ lib/WebHook/Slack.pm
@@ -0,0 +1,39 @@
+package WebHook::Slack;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebHook::Slack - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebHook::Slack;
+
+=head1 DESCRIPTION
+
+WebHook::Slack is ...
+
+=head1 LICENSE
+
+Copyright (C) nqounet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nqounet E<lt>mail@nqou.netE<gt>
+
+=cut
+
